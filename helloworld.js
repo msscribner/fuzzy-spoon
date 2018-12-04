e5791742ac53dd576fda15fcc639daf1f618ff4e
Shane Scribner <scribner.shane@gmail.com>
2018-12-03 22:11:12 -0500

    added website files

---
+++ helloworld.js
@@ -0,0 +1,19 @@
+//https://ui-router.github.io/ng1/tutorial/helloworld#add-script-tags
+var myApp = angular.module('helloworld', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
